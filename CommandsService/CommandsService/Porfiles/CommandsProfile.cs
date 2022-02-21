499e49f2ae91393c9631ba235817c649bc2be627
KarlGNassar <kgnassar01@gmail.com>
2022-02-21 09:46:06 +0200

    automapper profiles

---
+++ CommandsService/CommandsService/Porfiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.Porfiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+        }
+    }
+}
