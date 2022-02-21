2a60d5c9d154bfe234e601732d91edc6843e43a4
KarlGNassar <kgnassar01@gmail.com>
2022-02-21 09:31:21 +0200

    commands repository

---
+++ CommandsService/CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,22 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        void CreatePlatform(Platform platform);
+
+        bool PlatformExists(int platformId);
+
+
+        IEnumerable<Command> GetCommandsForPlarform(int platformId);
+
+        Command GetCommand(int platformId, int commandId);
+
+        void CreateCommand(int platformId, Command command);
+    }
+}
